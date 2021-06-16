a25618cf300394c5fbdcbcfe8b521fe07b40ba44
Kai <81066723+JavaScriptpy@users.noreply.github.com>
2021-06-16 20:59:31 +0800

    Update philippines.py

--- scripts/philippines.py
+++ scripts/philippines.py
@@ -17,31 +17,25 @@ class bcolors:
 apiKey = '588ab41cee2f41d0b946967069'
 
 # JSON Requester
-rpll = requests.get("https://api.checkwx.com/metar/RPLL?x-api-key=" + apiKey)
-rpll = requests.get("https://api.checkwx.com/metar/RPLL?x-api-key=" + apiKey)
-rpvm = requests.get("https://api.checkwx.com/metar/RPVM?x-api-key=" + apiKey)
+rpli = requests.get("https://api.checkwx.com/metar/RPLI?x-api-key=" + apiKey)
 rplc = requests.get("https://api.checkwx.com/metar/RPLC?x-api-key=" + apiKey)
 rplb = requests.get("https://api.checkwx.com/metar/RPLB?x-api-key=" + apiKey)
-rpli = requests.get("https://api.checkwx.com/metar/RPLI?x-api-key=" + apiKey)
+rpll = requests.get("https://api.checkwx.com/metar/RPLL?x-api-key=" + apiKey)
+rpvm = requests.get("https://api.checkwx.com/metar/RPVM?x-api-key=" + apiKey)
 rpvp = requests.get("https://api.checkwx.com/metar/RPVP?x-api-key=" + apiKey)
 rpvd = requests.get("https://api.checkwx.com/metar/RPVD?x-api-key=" + apiKey)
-rpmr = requests.get("https://api.checkwx.com/metar/RPMR?x-api-key=" + apiKey)
 rpmd = requests.get("https://api.checkwx.com/metar/RPMD?x-api-key=" + apiKey)
 rpmz = requests.get("https://api.checkwx.com/metar/RPMZ?x-api-key=" + apiKey)
+rpmr = requests.get("https://api.checkwx.com/metar/RPMR?x-api-key=" + apiKey)
 
 # Fetch Alert
 print("Fetching Latest METAR Report...")
 
 # fetch, parse and display METAR data
 try:
-    rpll.raise_for_status()
-    resp = rpll.json()
-    print(f"{bcolors.OKCYAN}Ninoy Aquino International Airport (Manila International Airport){bcolors.ENDC}")
-    print(resp['data'])
-
-    rpvm.raise_for_status()
-    resp = rpvm.json()
-    print(f"{bcolors.OKCYAN}Mactan Cebu International Airport{bcolors.ENDC}")
+    rpli.raise_for_status()
+    resp = rpli.json()
+    print(f"{bcolors.OKCYAN}Laoag International Airport{bcolors.ENDC}")
     print(resp['data'])
 
     rplc.raise_for_status()
@@ -54,9 +48,14 @@ try:
     print(f"{bcolors.OKCYAN}Subic Bay International Airport{bcolors.ENDC}")
     print(resp['data'])
 
-    rpli.raise_for_status()
-    resp = rpli.json()
-    print(f"{bcolors.OKCYAN}Laoag International Airport{bcolors.ENDC}")
+    rpll.raise_for_status()
+    resp = rpll.json()
+    print(f"{bcolors.OKCYAN}Ninoy Aquino International Airport (Manila International Airport){bcolors.ENDC}")
+    print(resp['data'])
+
+    rpvm.raise_for_status()
+    resp = rpvm.json()
+    print(f"{bcolors.OKCYAN}Mactan Cebu International Airport{bcolors.ENDC}")
     print(resp['data'])
 
     rpvp.raise_for_status()
@@ -66,7 +65,7 @@ try:
 
     rpvd.raise_for_status()
     resp = rpvd.json()
-    print(f"{bcolors.OKCYAN}Sibulan Airport (Dumaguete Airport){bcolors.ENDC}")
+    print(f"{bcolors.OKCYAN}Sibulan Airport{bcolors.ENDC}")
     print(resp['data'])
 
     rpmd.raise_for_status()
@@ -76,6 +75,11 @@ try:
 
     rpmz.raise_for_status()
     resp = rpmz.json()
+    print(f"{bcolors.OKCYAN}Zamboanga International Airport{bcolors.ENDC}")
+    print(resp['data'])
+
+    rpmr.raise_for_status()
+    resp = rpmr.json()
     print(f"{bcolors.OKCYAN}Zamboanga International Airport{bcolors.ENDC}")
     print(resp['data'])
 
